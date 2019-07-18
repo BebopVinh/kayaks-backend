bf298e25134f953c05b5ca99a935733dc7876a65
github username <github email address>
2019-07-18 15:43:30 -0400

    login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(name: params[:session][:name])
+    # @user = User.find_by(name: params[:name])
+    # if @user && @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user
+    else
+      render json: {
+        error: 'invalid credentials'
+      }
+    end
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: current_user
+    else
+      render json: {
+        error: 'no one logged in'
+      }
+    end
+  end
+
+
+
+end
